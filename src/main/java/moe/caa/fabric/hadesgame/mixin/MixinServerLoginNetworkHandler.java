91decc9a58021fa371f928c7cf53fb7927014786
CaaMoe <aeva_d@hotmail.com>
2025-06-20 19:06:09 +0800

    Refactor onHello method in MixinServerLoginNetworkHandler to improve event processing

--- src/main/java/moe/caa/fabric/hadesgame/mixin/MixinServerLoginNetworkHandler.java
+++ src/main/java/moe/caa/fabric/hadesgame/mixin/MixinServerLoginNetworkHandler.java
@@ -1,12 +1,9 @@
 package moe.caa.fabric.hadesgame.mixin;
 
-import moe.caa.fabric.hadesgame.event.EventsKt;
 import moe.caa.fabric.hadesgame.event.OnHello;
 import net.minecraft.network.packet.c2s.login.LoginHelloC2SPacket;
 import net.minecraft.server.network.ServerLoginNetworkHandler;
-import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,12 +11,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerLoginNetworkHandler.class)
 public abstract class MixinServerLoginNetworkHandler {
-    @Shadow
-    public abstract void disconnect(Text reason);
-
     @Inject(method = "onHello", at = @At("HEAD"), cancellable = true)
     private void onOnHello(LoginHelloC2SPacket packet, CallbackInfo ci) {
-        if(OnHello.Companion.process((ServerLoginNetworkHandler) (Object) this)){
+        if (OnHello.Companion.process((ServerLoginNetworkHandler) (Object) this)) {
             ci.cancel();
         }
     }
