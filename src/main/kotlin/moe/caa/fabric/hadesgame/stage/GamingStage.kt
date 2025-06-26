9a8806b0626489eb70950053a37d6f6af3e1b32c
CaaMoe <aeva_d@hotmail.com>
2025-06-26 14:00:29 +0800

    feat(stage): 增加游戏事件和边界缩小功能
    - 添加事件倒计时和随机事件选择功能
    - 实现边界缩小效果，从 900 缩小到 1
    -增加玩家存活数量显示
    - 优化游戏信息广播内容
    - 添加游戏结束时事件处理

--- src/main/kotlin/moe/caa/fabric/hadesgame/stage/GamingStage.kt
+++ src/main/kotlin/moe/caa/fabric/hadesgame/stage/GamingStage.kt
@@ -1,5 +1,7 @@
 package moe.caa.fabric.hadesgame.stage
 
+import moe.caa.fabric.hadesgame.GameCore
+import moe.caa.fabric.hadesgame.gameevent.AbstractGameEvent
 import moe.caa.fabric.hadesgame.handler.ScoreboardHandler
 import moe.caa.fabric.hadesgame.util.*
 import net.minecraft.sound.SoundEvents
@@ -12,10 +14,29 @@ data object GamingStage : AbstractStage() {
     override val stageName: String = "游戏中"
     override val nextStage: AbstractStage = EndStage
 
+    private var eventCountdownRange = 30..90
+
     private var tick = 0
     var invincibleCountdown = 0
 
     var winner: Text? = null
+
+    private var codType: CodType = CodType.ALL
+    private var eventCountdown = 0
+    private var event = AbstractGameEvent::class.sealedSubclasses.map { it.objectInstance!! }.random()
+
+    enum class CodType {
+        ALL,
+        COUNTDOWN,
+        NAME
+    }
+
+    private fun randomNext() {
+        eventCountdown = eventCountdownRange.random()
+        codType = CodType.entries.toTypedArray().random()
+        event = AbstractGameEvent::class.sealedSubclasses.map { it.objectInstance!! }.random()
+
+    }
 
     override suspend fun startStage() {
         tick = 0
@@ -26,6 +47,11 @@ data object GamingStage : AbstractStage() {
             player.resetState()
             player.changeGameMode(GameMode.SURVIVAL)
         }
+
+        val border = GameCore.server.overworld.worldBorder
+        border.size = 900.0
+
+        border.interpolateSize(900.0, 1.0, 1000 * 60 * 15)
     }
 
     override suspend fun tickStage() {
@@ -45,20 +71,59 @@ data object GamingStage : AbstractStage() {
                             .append(Text.literal(invincibleCountdown.toString()).withColor(Color.RED.rgb)).append("秒.")
                     )
                     add(Text.literal(" "))
+
+                    add(
+                        Text.literal(" 边界: ").append(
+                            Text.literal(GameCore.server.overworld.worldBorder.size.toInt().toString())
+                                .withColor(Color.RED.rgb)
+                        )
+                    )
+                    add(
+                        Text.literal(" 存活: ")
+                            .append(
+                                Text.literal(getPlayers().filter { it.gameMode != GameMode.SPECTATOR }.size.toString())
+                                .withColor(Color.RED.rgb)
+                            )
+                    )
+
+                    add(Text.literal(" "))
                     add(Text.literal("(╯°□°)╯").withColor(Color.YELLOW.rgb))
                 })
                 Text.literal("无敌时间还剩余 ").withColor(Color.LIGHT_GRAY.rgb)
                     .append(Text.literal(invincibleCountdown.toString()).withColor(Color.RED.rgb))
                     .append(Text.literal(" 秒").withColor(Color.LIGHT_GRAY.rgb)).broadcastOverlay()
+
             } else if (invincibleCountdown == 0) {
                 SoundEvents.GOAT_HORN_SOUNDS[2].value().broadcast(1000F, 1F)
                 Text.literal("鲨了他们!").withColor(Color.RED.rgb).broadcastOverlay()
-
+                randomNext()
             } else {
+
+                // todo 未完成
                 ScoreboardHandler.updateContents(contents = buildList {
                     add(Text.literal(DATE_FORMAT.format(LocalDateTime.now())).withColor(Color.LIGHT_GRAY.rgb))
                     add(Text.literal(" "))
-                    add(Text.literal(" 剩下的还没做完  "))
+                    add(Text.literal(" 下一事件:"))
+                    add(
+                        Text.literal("   ")
+                            .append(Text.literal("§k交换位置").withColor(Color.GREEN.rgb))
+                            .append(Text.literal("  ").withColor(Color.GREEN.rgb))
+                            .append(Text.literal("§k00:15").withColor(Color.LIGHT_GRAY.rgb))
+                    )
+                    add(Text.literal(" "))
+                    add(
+                        Text.literal(" 边界: ").append(
+                            Text.literal(GameCore.server.overworld.worldBorder.size.toInt().toString())
+                                .withColor(Color.RED.rgb)
+                        )
+                    )
+                    add(
+                        Text.literal(" 存活: ")
+                            .append(
+                                Text.literal(getPlayers().filter { it.gameMode != GameMode.SPECTATOR }.size.toString())
+                                .withColor(Color.RED.rgb)
+                            )
+                    )
                     add(Text.literal(" "))
                     add(Text.literal("(╯°□°)╯").withColor(Color.YELLOW.rgb))
                 })
@@ -78,5 +143,9 @@ data object GamingStage : AbstractStage() {
         }
 
         return false
+    }
+
+    override suspend fun endStage() {
+        AbstractGameEvent::class.sealedSubclasses.map { it.objectInstance!! }.map { it.endEvent() }
     }
 }
