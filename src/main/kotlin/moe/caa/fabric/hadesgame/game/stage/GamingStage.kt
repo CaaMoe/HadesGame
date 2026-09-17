bf30812826cde86ce1169bf460b8a1c41937090f
CaaMoe <aeva_d@hotmail.com>
2026-09-17 18:59:06 +0800

    调整事件倒计时范围

--- src/main/kotlin/moe/caa/fabric/hadesgame/game/stage/GamingStage.kt
+++ src/main/kotlin/moe/caa/fabric/hadesgame/game/stage/GamingStage.kt
@@ -21,7 +21,7 @@ data object GamingStage : AbstractStage() {
     val allGameEvents = AbstractGameEvent.fetchAllGameEvents()
     private val extractableEvents = allGameEvents.filter { it.extractable }
 
-    private var eventCountdownRange = 20..70
+    private var eventCountdownRange = 30..120
     private var tickNumber = 0
 
     private var currentEvents = emptyList<EventCarrier>()
